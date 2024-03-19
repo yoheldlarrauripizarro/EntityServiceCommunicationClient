3f5ffdf64de1555a3142ef15bd1966b335dc82ff
Yohel D. Larrauri Pizarro <yohel.larrpizarro@gmail.com>
2024-03-19 00:40:41 -0300

    feat: update MainActivity file adding a serviceconnection

--- app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunicationclient/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunicationclient/MainActivity.kt
@@ -1,6 +1,16 @@
 package br.edu.ifsp.scl.sdm.entityservicecommunicationclient
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
+import android.os.Message
+import android.os.Messenger
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.sdm.entityservicecommunicationclient.databinding.ActivityMainBinding
 
@@ -9,17 +19,73 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private lateinit var incrementBoundServiceIntent: Intent
+    private var counter = 0
+    private lateinit var ibsMessenger: Messenger
+
+    private val incrementBoundServiceConnection = object : ServiceConnection{
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            Log.v(getString(R.string.app_name),"Client bound to the service.")
+            service?.also {
+                ibsMessenger = Messenger(service)
+
+                ibsMessenger.send(Message.obtain().apply {
+                    Messenger(object : Handler(Looper.myLooper()!!){
+                        override fun handleMessage(msg: Message) {
+                            super.handleMessage(msg)
+                            counter = msg.data.getInt("VALUE")
+                            Toast.makeText(this@MainActivity,
+                                "You Clicked $counter times.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }).also{messenger ->
+                        replyTo = messenger
+                    }
+                })
+            }
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            Log.v(getString(R.string.app_name),"Client unbound to the service.")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        incrementBoundServiceIntent = Intent().apply {
+            component = ComponentName(
+                "br.edu.ifsp.scl.sdm.entityservicecommunicaction",
+                    "br.edu.ifsp.scl.sdm.entityservicecommunicaction.IncrementBoundService"
+            )
+        }
+
+        if (!bindService(
+            incrementBoundServiceIntent,
+            incrementBoundServiceConnection,
+            BIND_AUTO_CREATE
+            )){
+            Toast.makeText(this,"Service unavailable", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
         with(amb) {
             mainTb.apply {
                 getString(R.string.app_name).also { setTitle(it) }
                 setSupportActionBar(this)
             }
             incrementBt.setOnClickListener {
-
+                ibsMessenger.send(Message.obtain().apply{
+                    data.putInt("VALUE", counter)
+                })
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unbindService(incrementBoundServiceConnection)
     }
 }
